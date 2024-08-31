825f3d59a5667176f85e60e1ceec51f024fb51a7
notosgnikusnsi <notosgnikusnsi@gmail.com>
2024-08-31 23:26:50 +0900

    feat: display the app title

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { ChakraBaseProvider, Text } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
   return (
     <>
+      <ChakraBaseProvider>
+        <Text fontSize={"x-large"}>学習記録一覧</Text>
+      </ChakraBaseProvider>
     </>
   )
 }
